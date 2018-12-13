bd92b983c41f2cb889580f3cbd6ca60a6912e786
hammy <44582470+Merza-hammy@users.noreply.github.com>
2018-12-13 12:26:34 -0600

    Update server.py

--- server.py
+++ server.py
@@ -13,7 +13,7 @@ def index():
   data = json.loads(request.get_data())
 
     #Get City
-    ccrypto_name = data['conversation']['memory']['location']['value']
+    city = data['conversation']['memory']['location']['value']
     #Fetch Weather Data
     r = requests.get("https://api.apixu.com/v1/current.json?key=9a48c907e1534875947150810181312&q="+city)
 
