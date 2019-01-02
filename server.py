8ad6cbaacb19f0bed0a28306df03b7a33b4f3c43
hammy <44582470+Merza-hammy@users.noreply.github.com>
2019-01-02 13:23:01 -0600

    Update server.py

--- server.py
+++ server.py
@@ -1,36 +1,20 @@
-# coding=utf-8
-from flask import Flask, request, jsonify
-import json
-import requests
-import objectpath
-import os
- 
+from flask import Flask
 app = Flask(__name__)
-port = int(os.environ["PORT"]) #definicion de puerto de salida
- 
-#@app.route('/', methods=['POST'])
 
-#def index():
+@app.route('/')
+                
+
+
+ if intent == "sumar" and skill == "suma": #Si el intent y el skill es el esperado
   
-                #postRece = json.loads(request.get_data())
-                #intent = str(postRece['nlp']['sumar'][0]['slug']) #Se obtiene el nombre del intent desde el post de Recast
-                #skill = str(postRece['conversation']['suma']) #Se obtiene el nombre del skill desde el post de Recast
-                #print(skill)
-                #print(intent)
-                if intent == "sumar" and skill == "suma": #Si el intent y el skill es el esperado
-                                              
-                numero1=float
-                numero2=float
-               suma= numero1+numero2
-               print(str(suma))
+def hello_world():
+    return 'Hola ponchis'
+
+if __name__ == '__main__':
+    
 
                
                
-               
+          
 
- 
-#cf login
-#API Endpoint:https://api.cf.eu10.hana.ondemand.com
-#cf push botRecast manifest file C:\Users\i861443\Desktop\python SCP\Recast\manifest.yml
-#requirements.txt para instalación de imports
 
